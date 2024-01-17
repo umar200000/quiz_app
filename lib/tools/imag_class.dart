f5b1680bc309eaf9e0af2f9721bf97fa84588e58
umar200000 <ruzievumar19@gmail.com>
2024-01-17 22:24:19 +0500

    it is good now

--- lib/tools/imag_class.dart
+++ lib/tools/imag_class.dart
@@ -4,4 +4,5 @@ class Images100 {
   static const mainImg = "assets/images/img_1.png";
   static const mathImg = "assets/images/math.png";
   static const dartImg = "assets/images/Group.png";
+  static const englishImage = "assets/images/Group.png";
 }
